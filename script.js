46d214a30e41e485af22c38120bdd456ce021fda
Khun Myo Myat <hanmyo551@gmail.com>
2025-03-16 20:20:08 +0700

    add action to buttons

---
+++ script.js
@@ -0,0 +1,19 @@
+(function () {
+  const buttons = document.querySelectorAll(".btn-counter");
+  let count = 0;
+
+  const counter = document.querySelector(".counter-process");
+  //   const counterContainer = document.querySelector(".counter");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      if (button.classList.contains("decrease-btn")) {
+        count--;
+      } else if (button.classList.contains("increase-btn")) {
+        count++;
+      }
+
+      counter.textContent = count;
+    });
+  });
+})();
